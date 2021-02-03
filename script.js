4cdedace5b69b898907023f89947e76fecff48eb
H. Burdy <58144965+hburdy@users.noreply.github.com>
2021-02-03 17:19:57 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggle() {
+    //Learning to Toggle
+    let myElement = document.getElementById('id1');
+    if (myElement.style.backgroundColor == 'red') {
+    myElement.style.backgroundColor = 'yellow';
+    myElement.style.color = 'black';
+    } else {
+    myElement.style.backgroundColor = 'red';
+    myElement.style.color = 'white';
+    }
+}
+
+window.onload = function() {
+    document.getElementById('btn1').onclick = toggle;
+}
